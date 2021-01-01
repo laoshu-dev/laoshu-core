21ffc21408bb4e14c96fa15ed9ab544da680df5b
Anton Tkalikov <atkalikov@gmail.com>
2021-01-01 16:35:22 +0300

    Change name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "laoshu-models",
+    name: "LaoshuModels",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
