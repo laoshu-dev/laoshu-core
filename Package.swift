d0b60474400adba3f34b81ea9eaddb3dff8569cf
Anton Tkalikov <atkalikov@gmail.com>
2021-02-28 20:47:06 +0300

    Make static library

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
     products: [
         .library(
             name: "LaoshuModels",
-            type: .dynamic,
             targets: ["LaoshuModels"]),
     ],
     targets: [
