f8ed0a63babb6f782de994b2f22b47d25b2ff9b3
Anton Tkalikov <atkalikov@gmail.com>
2021-02-28 19:36:56 +0300

    Comment UI-related model

--- Sources/LaoshuModels/Models/Core/MailContainer.swift
+++ Sources/LaoshuModels/Models/Core/MailContainer.swift
@@ -6,20 +6,20 @@
 //  Copyright © 2020 Anton Tkalikov. All rights reserved.
 //
 
-#if os(iOS)
-import UIKit
-public typealias TargetedViewController = UIViewController
-#elseif os(macOS)
-import AppKit
-public typealias TargetedViewController = NSViewController
-#endif
-
-public struct MailContainer {
-    public let url: URL
-    public let controller: TargetedViewController?
-    
-    public init(url: URL, controller: TargetedViewController?) {
-        self.url = url
-        self.controller = controller
-    }
-}
+//#if os(iOS)
+//import UIKit
+//public typealias TargetedViewController = UIViewController
+//#elseif os(macOS)
+//import AppKit
+//public typealias TargetedViewController = NSViewController
+//#endif
+//
+//public struct MailContainer {
+//    public let url: URL
+//    public let controller: TargetedViewController?
+//
+//    public init(url: URL, controller: TargetedViewController?) {
+//        self.url = url
+//        self.controller = controller
+//    }
+//}
